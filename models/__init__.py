883fb28cfd0c8fd24c9e359c7b10528f5d9ca23d
X. Zhou <153439738+bzfakkzz@users.noreply.github.com>
2026-01-25 00:33:40 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,26 +1,17 @@
 from .vgg16 import TorchVGG16
-from .lstm0 import TorchLSTMModel0
 from .alexnet import TorchAlexNetCIFAR10
 from .densenet import TorchDenseNet
-from .inceptionblock import TorchInceptionBlock
 from .lenet5fashion import TorchLeNet5Fashion
 from .lenet5mnist import TorchLeNet5
-from .lstm2 import TorchLSTMModel2
-from .mobilenet import TorchMobileNet
 from .resnet50 import TorchResNet50
-from .vgg19 import TorchVGG19
 from .xception import TorchXception
 
 __all__=[
     'TorchVGG16',
-    'TorchLSTMModel0',
     'TorchAlexNetCIFAR10',
     'TorchDenseNet',
-    'TorchInceptionBlock',
     'TorchLeNet5Fashion',
     'TorchLeNet5',
-    'TorchMobileNet',
     'TorchResNet50',
-    'TorchVGG19',
     'TorchXception'
 ]
